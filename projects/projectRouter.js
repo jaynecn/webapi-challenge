9942aef21ff60edf7b9a3ce22bb25b788f201a62
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-11-01 09:55:12 +0000

    completed POST request on projectRouter

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -4,15 +4,49 @@ const db = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
-// GET request
-router.get('', (req, res) => {
-  db.get(req.query)
+// GET requests
+router.get('/', (req, res) => {
+  db.get(req.body)
   .then(data => {
     res.status(200).json(data);
   })
   .catch(error => {
-    res.status(500).json({ error: 'The posts information could not be retrieved. ' + error.message })
+    res.status(500).json({ error: 'The project information could not be retrieved. ' + error.message })
   })
 });
+
+router.get('/:id', (req, res) => {
+  db.get(req.params.id)
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(error => {
+    res.status(500).json({ error: 'The project information could not be retrieved. ' + error.message})
+  })
+});
+
+// POST request
+router.post('/', (req, res) => {
+  const post = {name: req.body.name, description: req.body.description};
+   db.insert(post)
+  .then(data => {
+    res.status(201).json(data);
+  })
+  .catch(error => {
+    res.status(400).json({errorMessage: "Please provide all required details for the project." + error.message})
+  })
+})
+
+// DELETE request
+router.delete('/', (req, res) => {
+  db.remove(req.params.id)
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(error => {
+    res.status(500).json({ error: 'The post could not be removed ' + error.message});
+  })
+});
+
 
 module.exports = router;
